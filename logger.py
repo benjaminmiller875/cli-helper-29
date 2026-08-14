20cc7905a28ece1f6f7baf3583e94867bfc33f84
Benjamin Miller <benjaminmiller875@gmail.com>
2026-08-14 15:16:07 +0200

    Add logger implementation

--- logger.py
+++ logger.py
@@ -1,13 +1,32 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file, max_bytes=5 * 1024 * 1024, backup_count=3):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    return logger
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+logger = Logger(__name__)
+
+# Example usage
+if __name__ == '__main__':
+    logger.info('Logger initialized')
